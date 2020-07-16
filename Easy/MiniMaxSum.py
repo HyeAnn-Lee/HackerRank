06576b50bf639880452db3878da25d723f0b82f8
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-07-16 14:49:29 +0900

    Update MiniMaxSum.py

--- Easy/MiniMaxSum.py
+++ Easy/MiniMaxSum.py
@@ -8,17 +8,11 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    mini = arr[0]
-    maxi = arr[0]
-    for ele in arr:
-        if ele < mini:
-            mini = ele
-        if ele > maxi:
-            maxi = ele
     Sum = sum(arr)
-    print(f'{Sum-maxi} {Sum-mini}')
+    print(f'{Sum - max(arr)} {Sum - min(arr)}')
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
 
     miniMaxSum(arr)
+
