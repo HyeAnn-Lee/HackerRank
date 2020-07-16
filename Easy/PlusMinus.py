7956a9ef5e1e252c2adfbb999196371b93e9e0b9
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-07-16 13:00:50 +0900

    Create PlusMinus.py

---
+++ Easy/PlusMinus.py
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/plus-minus/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    ps = 0
+    ng = 0
+    zr = 0
+    for i in range(n):
+        if arr[i] > 0:
+            ps += 1
+        elif arr[i] == 0:
+            zr += 1
+        else:
+            ng += 1
+    print('{:.6f}'.format(ps/n))
+    print('{:.6f}'.format(ng/n))
+    print('{:.6f}'.format(zr/n))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
